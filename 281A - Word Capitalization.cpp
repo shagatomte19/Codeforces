3327fa133e16d89656432f8c931e79b396431621
Enamul Hasan Shagato <82907635+shagatomte19@users.noreply.github.com>
2022-04-27 07:15:00 +0600

    3 files added

---
+++ 281A - Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string word;
+
+    cin >> word;
+
+    word[0] = towupper(word[0]);
+
+    cout<<word<<endl;
+
+    return 0;
+}
