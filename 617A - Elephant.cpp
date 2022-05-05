d87dda24ab70a42836054fba0dc20ea2e8120cd9
Enamul Hasan Shagato <82907635+shagatomte19@users.noreply.github.com>
2022-05-05 14:39:03 +0600

    Add files via upload

---
+++ 617A - Elephant.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+   int n;
+   cin>>n;
+
+   if(n<=5 && n>0)
+    cout<<"1";
+   else if(n>5 && n%5!=0)
+    cout<<n/5+1;
+   else if(n>5 && n%5==0)
+    cout<<n/5;
+}
